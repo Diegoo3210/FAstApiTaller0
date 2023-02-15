0672185f69024e630966af2a9ac820c1e910f45f
Diegoo3210 <alejito3210@icloud.com>
2023-02-15 00:32:21 -0500

    Pequeños cambios

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import requirements as r
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
@@ -11,12 +21,12 @@ def index():
 @app.get("/getRequest/A")
 def mostrar_A():
     numWords = r.question_a("./dataTesting/reut2-000.sgm")
-    return ([{"sol": numWords}])
+    return ({"sol": numWords})
 
 @app.get("/getRequest/B")
 def mostrar_B():
     numWordsSeparate = r.question_b("./dataTesting/reut2-000.sgm")
-    return numWordsSeparate
+    return {"sol":numWordsSeparate}
 
 @app.get("/getRequest/C/{n}")
 def mostrar_C(n:int):
@@ -24,7 +34,7 @@ def mostrar_C(n:int):
     return ({"sol": numWords})
 
 @app.get("/getRequest/D/{name}/{n}")
-def mostrar_libro(name, n):
-    numWords = r.question_cd("reut2-000.sgm", n)
+def mostrar_libro(name:str, n):
+    numWords = r.question_cd("./dataTesting/"+{name}, n)
     return ({"sol": numWords})
 
